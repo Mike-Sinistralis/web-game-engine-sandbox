b061b7c776ecd76ab6702dfc059fe866d92628c0
Bearmetal <44550489+ProdigalNerd@users.noreply.github.com>
2023-05-09 21:26:02 -0400

    switch to react-babylon to handle most of the components

--- src/Babylon/index.jsx
+++ src/Babylon/index.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-vars */
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { useEffect, useRef } from 'react';
-import { init } from './init';
-import { teardown } from './teardown';
+import {ArcRotateCamera, Engine, Scene, HemisphericLight } from "react-babylonjs";
+import { Color3, Vector3 } from '@babylonjs/core';
+import SpinningBox from "./spinning-box.jsx";
 
 const styles = {
   canvas: css({
@@ -16,20 +16,38 @@ const styles = {
 };
 
 function Babylon() {
-  const reactCanvas = useRef(null);
-
-  useEffect(() => {
-    if (reactCanvas.current) {
-      init(reactCanvas.current);
-    }
-
-    return () => {
-      teardown();
-    };
-  }, [reactCanvas]);
 
   return (
-    <canvas ref={reactCanvas} css={styles.canvas} />
+      <div>
+        <Engine antialias adaptToDeviceRatio canvasId="babylonJS" css={styles.canvas}>
+          <Scene>
+            <ArcRotateCamera
+                name="camera1"
+                target={Vector3.Zero()}
+                alpha={Math.PI / 2}
+                beta={Math.PI / 4}
+                radius={8}
+            />
+            <HemisphericLight
+                name="light1"
+                intensity={0.7}
+                direction={Vector3.Up()}
+            />
+            <SpinningBox
+                name="left"
+                position={new Vector3(-2, 0, 0)}
+                color={Color3.FromHexString('#EEB5EB')}
+                hoveredColor={Color3.FromHexString('#C26DBC')}
+            />
+            <SpinningBox
+                name="right"
+                position={new Vector3(2, 0, 0)}
+                color={Color3.FromHexString('#C8F4F9')}
+                hoveredColor={Color3.FromHexString('#3CACAE')}
+            />
+          </Scene>
+        </Engine>
+      </div>
   );
 }
 
